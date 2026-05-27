506e1525c55e8aa92177bc7418fbdbff2927d1ab
Alexandre Carvalho <alexandre.carvalho@live.com>
2026-05-26 22:09:55 -0300

    Criação da estrutura do projeto baseado em Clean Architecture e criação das classes de domínio User e Address.

---
+++ src/ShopFlexByte.Domain/Entities/User.cs
@@ -0,0 +1,35 @@
+﻿namespace ShopFlexByte.Domain.Entities;
+
+public sealed class User(string username, string email, string fullName, Address primaryAddress)
+{
+    public Guid Id { get; private set; } = Guid.NewGuid();
+    public string Username { get; } = username;
+    public string Email { get; } = email;
+    public string FullName { get; } = fullName;
+    public Address PrimaryAddress { get; private set; } = primaryAddress;
+    private readonly List<Address> _otherAddresses = new();
+    public IReadOnlyCollection<Address> OtherAddresses => _otherAddresses.AsReadOnly();
+
+    //public User(string username, string email, string fullName, Address primaryAddress)
+    //{
+    //    Username = username;
+    //    Email = email;
+    //    FullName = fullName;
+    //    PrimaryAddress = primaryAddress;
+    //}
+
+    public void SetPrimaryAddress(Address address)
+    {
+        PrimaryAddress = address;
+    }
+
+    public void AddOtherAddress(Address address)
+    {
+        _otherAddresses.Add(address);
+    }
+
+    public void RemoveOtherAddress(Address address)
+    {
+        _otherAddresses.Remove(address);
+    }
+}
